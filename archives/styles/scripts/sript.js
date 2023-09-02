644bc9f4a09d9a357cd8151d2710f9abbe8b969c
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2023-09-02 13:38:08 -0300

    0.3
    -menu pronto;

--- archives/styles/scripts/sript.js
+++ archives/styles/scripts/sript.js
@@ -1,7 +1,8 @@
 function menu(){
-    if(menu.style.display === "none"){
-        menu.style.display == "block";
+    let menu = document.getElementById("menu")
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block';
     }else{
-        menu.style.display == "none";
+        menu.style.display = 'none';
     }
 }
